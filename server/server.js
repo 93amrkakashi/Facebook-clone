1984e92b378c62fb96d431d6e715dc77431677bd
93amr.kakashi <93amr.kakashi@gmail.com>
2023-04-26 00:34:51 +0700

    added graphql

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
-console.log("first")
+const express = require("express")
+const { graphqlHTTP } = require("express-graphql")
+const app = express();
+
+app.use(express.json());
+app.use((req, res, next) => {
+
+  next();
+});
+
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
+app.listen(process.env.PORT, () =>{
+  console.log("first")
+})
